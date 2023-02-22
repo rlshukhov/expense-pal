6a7b0656886572b7030fc1ab215426c301cccb57
Lane Shukhov <rl.shukhov@icloud.com>
2023-02-22 05:14:24 +0300

    Initial Commit

---
+++ budget/budgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetApp.swift
+//  budget
+//
+//  Created by Lane Shukhov on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct budgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
